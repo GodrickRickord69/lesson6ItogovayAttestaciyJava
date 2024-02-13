424773878ced04b6939479c0a000e5f4333eacd7
Nikita <varashilov125@gmail.com>
2024-02-13 21:38:47 +0300

    PhoneBookModDostup

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,32 +1,32 @@
 import java.util.*;
 
 public class PhoneBook {
-    Map<String, Contact> contacts;
+    private Map<String, Contact> contacts;
 
-    PhoneBook(){
+    public PhoneBook(){
         contacts = new HashMap<>();
     }
 
-    void addContact(String name, String phoneNumber){
+    public void addContact(String name, String phoneNumber){
         if (contacts.containsKey(name)){
             contacts.get(name).addPhoneNomber(phoneNumber);
         } else {
             Contact contact = new Contact(name, phoneNumber);
             contacts.put(name, contact);
         }
-    void removeContact(String name){
+    public void removeContact(String name){
             contacts.remove(name);
         }
-    void removePhoneNumber(String name, String phoneNumber) {
+    public void removePhoneNumber(String name, String phoneNumber) {
             if (contacts.containsKey(name)) {
                 contacts.get(name).removePhoneNumbers(phoneNumber);
             }
         }
-    void displayContacts() {
+    public void displayContacts() {
             List<Contact> sortedContacts = new ArrayList<>(contacts.values());
             sortedContacts.sort(Comparator.comparingInt(contact -> -contact.getPhoneNumbers().size()));
             for (Contact contact: sortedContacts){
-                System.out.println(contact.getUser_name() + ":" + contact.getPhoneNumbers());
+                System.out.println(contact.getName() + ":" + contact.getPhoneNumbers());
             }
         }
     }
